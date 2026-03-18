403db45624e59ef281b66438f0114e038eb71f0c
ADITYA02-coder <as9025524@gmail.com>
2026-03-18 21:26:07 +0530

    Add_shared_password

--- server.js
+++ server.js
@@ -14,9 +14,27 @@ const io = new Server(server, {
 
 app.use(express.static(__dirname));
 
+const CHAT_PASSWORD = process.env.CHAT_PASSWORD;
+
+io.use((socket, next) => {
+  if (!CHAT_PASSWORD) {
+    return next(new Error("Server password not set"));
+  }
+  const { password } = socket.handshake.auth || {};
+  if (password === CHAT_PASSWORD) {
+    return next();
+  }
+  return next(new Error("Invalid password"));
+});
+
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
+  });
+  io.emit("online users", io.engine.clientsCount);
+
+  socket.on("disconnect", () => {
+    io.emit("online users", io.engine.clientsCount);
   });
 });
 
